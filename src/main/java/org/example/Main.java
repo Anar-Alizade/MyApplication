20e55047093102f72c926b2dcf0abb2c4f169e69
alizadaar <Anar.Alizada@kapitalbank.az>
2026-01-23 12:21:56 +0400

    message to anar

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+        System.out.println("salam Anar");
+
+        int a = 10;
+        int b = 20;
+    }
+}
